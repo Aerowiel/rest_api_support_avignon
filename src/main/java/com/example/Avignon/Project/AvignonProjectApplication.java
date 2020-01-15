79e0df5c0adabfe5f1eb470ebce943eed9d14d0d
Florian PENDARIES <florian.pendaries@hotmail.fr>
2020-01-15 12:13:22 +0100

    initial commit

---
+++ src/main/java/com/example/Avignon/Project/AvignonProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Avignon.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvignonProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvignonProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Avignon/Project/AvignonProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Avignon.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvignonProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
